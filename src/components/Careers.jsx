b4d3cb2b1bca6ade79da7662bf08e4fbbc2791b8
Hafizul Haque <hafizul.haque@reddotdigitalit.com>
2023-01-10 13:19:27 +0600

    new commit after career

---
+++ src/components/Careers.jsx
@@ -0,0 +1,14 @@
+//loader function
+const careersLoader = async () => {
+  
+}
+
+const Careers = () => {
+  return (
+    <div className='careers'>
+      all ther careers!
+    </div>
+  )
+}
+
+export default Careers
